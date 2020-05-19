6547ebbd81e25a023963378e3b8d13d0ad6d6413
Declan k <decktehboss@hotmail.com>
2020-05-19 15:58:07 -0400

    g

--- Sorting Efficiencies/Form1.Designer.cs
+++ Sorting Efficiencies/Form1.Designer.cs
@@ -28,40 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.originalOutput = new System.Windows.Forms.Label();
             this.sortedOutput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(308, 169);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
             // 
             // originalOutput
             // 
-            this.originalOutput.Location = new System.Drawing.Point(12, 21);
+            this.originalOutput.Location = new System.Drawing.Point(12, 69);
             this.originalOutput.Name = "originalOutput";
-            this.originalOutput.Size = new System.Drawing.Size(272, 417);
+            this.originalOutput.Size = new System.Drawing.Size(272, 369);
             this.originalOutput.TabIndex = 1;
             // 
             // sortedOutput
             // 
-            this.sortedOutput.Location = new System.Drawing.Point(494, 21);
+            this.sortedOutput.Location = new System.Drawing.Point(505, 69);
             this.sortedOutput.Name = "sortedOutput";
-            this.sortedOutput.Size = new System.Drawing.Size(272, 417);
+            this.sortedOutput.Size = new System.Drawing.Size(272, 369);
             this.sortedOutput.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(617, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sorted List";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(328, 69);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(38, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "10";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(328, 92);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(44, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "100";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(328, 115);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(50, 17);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "1000";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(328, 138);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(50, 17);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "5000";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.sortedOutput);
             this.Controls.Add(this.originalOutput);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -70,10 +147,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label originalOutput;
         private System.Windows.Forms.Label sortedOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Sorting Efficiencies/Form1.cs
+++ Sorting Efficiencies/Form1.cs
@@ -12,41 +12,38 @@ namespace Sorting_Efficiencies
 {
     public partial class Form1 : Form
     {
-       
-    
+        //int n = 100;
+        int[] originalArray = new int[100];
+        int[] sortedArray = new int[100];
+
+        int[] originalArray2 = new int[100];
+        int[] sortedArray2 = new int[100];
+
+        int[] originalArray3 = new int[100];
+        int[] sortedArray3 = new int[100];
         public Form1()
         {
             InitializeComponent();
 
             //int n = Convert.ToInt32(textBox1.Text);
-            int n = 100;
-            int[] originalArray = new int[n];
-            //int[] originalArray = { 3, 6, 1, 8, 5, 2 };
-            //int[] sortedArray = new int[6];
-            int[] sortedArray = new int[n];
-           
-          
-            Selection(sortedArray);
-            //bubble(sortedArray);
-            //insertion(sortedArray);
-            originalOutput.Text = sortedOutput.Text = "";
-
-
-            foreach (int i in originalArray)
-            {
-                originalOutput.Text += i + "\n";
-            }
-            foreach (int i in sortedArray)
-            {
-                sortedOutput.Text += i + "\n";
-            }
 
             Random randGen = new Random();
-            for (int i = 0; i<originalArray.Length; i++)
+            for (int i = 0; i < originalArray.Length; i++)
             {
-                originalArray[i] = randGen.Next(-1000,1001);
+                originalArray[i] = randGen.Next(-1000, 1001);
             }
             originalArray.CopyTo(sortedArray, 0);
+            originalArray.CopyTo(sortedArray2, 0);
+            originalArray.CopyTo(sortedArray3, 0);
+            originalArray.CopyTo(originalArray2, 0);
+            originalArray.CopyTo(originalArray3, 0);
+            Selection(sortedArray);
+            Display();
+            //bubble(sortedArray);
+            //insertion(sortedArray);
+         
+
+            
         }
         public void Selection(int[] tempArray)
         {
@@ -64,6 +61,45 @@ namespace Sorting_Efficiencies
                     }
                 }
             }
+        }
+        public void Display()
+        {
+            originalOutput.Text = sortedOutput.Text = "";
+
+
+            foreach (int i in originalArray)
+            {
+                originalOutput.Text += i + "\n";
+            }
+            foreach (int i in sortedArray)
+            {
+                sortedOutput.Text += i + "\n";
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            int n = 1000;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            int n = 5000;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            int n = 10;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            int n = 100;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
