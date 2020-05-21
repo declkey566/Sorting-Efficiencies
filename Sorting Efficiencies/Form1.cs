ea7c6b0ba381f20edeb8e778949741429dbea1c7
Declan k <decktehboss@hotmail.com>
2020-05-21 16:53:01 -0400

    g

--- Sorting Efficiencies/Form1.cs
+++ Sorting Efficiencies/Form1.cs
@@ -12,37 +12,19 @@ namespace Sorting_Efficiencies
 {
     public partial class Form1 : Form
     {
-        //int n = 100;
-        int[] originalArray = new int[100];
-        int[] sortedArray = new int[100];
+        int n;
+        int[] originalArray;
+        int[] sortedArray;
 
-        int[] originalArray2 = new int[100];
-        int[] sortedArray2 = new int[100];
+        int[] originalArray2;
+        int[] sortedArray2;
 
-        int[] originalArray3 = new int[100];
-        int[] sortedArray3 = new int[100];
+        int[] originalArray3;
+        int[] sortedArray3;
         public Form1()
         {
+            
             InitializeComponent();
-
-            //int n = Convert.ToInt32(textBox1.Text);
-
-            Random randGen = new Random();
-            for (int i = 0; i < originalArray.Length; i++)
-            {
-                originalArray[i] = randGen.Next(-1000, 1001);
-            }
-            originalArray.CopyTo(sortedArray, 0);
-            originalArray.CopyTo(sortedArray2, 0);
-            originalArray.CopyTo(sortedArray3, 0);
-            originalArray.CopyTo(originalArray2, 0);
-            originalArray.CopyTo(originalArray3, 0);
-            Selection(sortedArray);
-            Display();
-            //bubble(sortedArray);
-            //insertion(sortedArray);
-         
-
             
         }
         public void Selection(int[] tempArray)
@@ -65,40 +47,66 @@ namespace Sorting_Efficiencies
         public void Display()
         {
             originalOutput.Text = sortedOutput.Text = "";
-
-
-            foreach (int i in originalArray)
-            {
-                originalOutput.Text += i + "\n";
-            }
-            foreach (int i in sortedArray)
+                for (int i = 0; i < n; i++)
+                {
+                    originalOutput.Text += i + "\n";
+                }
+            for (int i = 0; i < n; i++)
             {
                 sortedOutput.Text += i + "\n";
             }
+            // foreach (int i in sortedArray)
+            // {
+            //      if (originalArray.Length < n)
+            //      {
+            //         originalOutput.Text += i + "\n";
+            //     }
+            // }
+            // foreach (int i in sortedArray)
+            // {
+            //      if (originalArray.Length < n)
+            //      {
+            //         sortedOutput.Text += i + "\n";
+            //     }
+            // }
         }
 
         private void checkBox3_CheckedChanged(object sender, EventArgs e)
         {
-            int n = 1000;
+             n = 1000;
         }
 
         private void checkBox4_CheckedChanged(object sender, EventArgs e)
         {
-            int n = 5000;
+             n = 5000;
         }
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
-            int n = 10;
+             n = 10;
+
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            int n = 100;
+             n = 100;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            Random randGen = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                originalArray[i] = randGen.Next(-1000, 1001);
+            }
+            originalArray.CopyTo(sortedArray, 0);
+            originalArray.CopyTo(sortedArray2, 0);
+            originalArray.CopyTo(sortedArray3, 0);
+            originalArray.CopyTo(originalArray2, 0);
+            originalArray.CopyTo(originalArray3, 0);
+            Selection(sortedArray);
+            Display();
 
         }
     }
