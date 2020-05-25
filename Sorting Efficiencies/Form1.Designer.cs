c059c6e024670b4862916688cf9444fafef66d2c
Declan k <decktehboss@hotmail.com>
2020-05-25 14:35:44 -0400

    hu

--- Sorting Efficiencies/Form1.Designer.cs
+++ Sorting Efficiencies/Form1.Designer.cs
@@ -37,26 +37,42 @@
             this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.checkBox4 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // originalOutput
             // 
             this.originalOutput.Location = new System.Drawing.Point(12, 69);
             this.originalOutput.Name = "originalOutput";
-            this.originalOutput.Size = new System.Drawing.Size(272, 369);
+            this.originalOutput.Size = new System.Drawing.Size(117, 369);
             this.originalOutput.TabIndex = 1;
             // 
             // sortedOutput
             // 
-            this.sortedOutput.Location = new System.Drawing.Point(505, 69);
+            this.sortedOutput.Location = new System.Drawing.Point(169, 70);
             this.sortedOutput.Name = "sortedOutput";
-            this.sortedOutput.Size = new System.Drawing.Size(272, 369);
+            this.sortedOutput.Size = new System.Drawing.Size(120, 369);
             this.sortedOutput.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(90, 25);
+            this.label1.Location = new System.Drawing.Point(44, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 3;
@@ -65,7 +81,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(617, 25);
+            this.label2.Location = new System.Drawing.Point(203, 37);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 4;
@@ -117,7 +133,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(328, 187);
+            this.button1.Location = new System.Drawing.Point(318, 187);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 9;
@@ -125,11 +141,147 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(580, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Information";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(467, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Selection Sort";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(694, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Bubble Sort";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(566, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Insertion Sort";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(420, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(158, 26);
+            this.label7.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(420, 129);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(158, 26);
+            this.label8.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(420, 164);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(158, 26);
+            this.label9.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(420, 192);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(158, 26);
+            this.label10.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(640, 192);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(158, 26);
+            this.label11.TabIndex = 21;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(640, 164);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(158, 26);
+            this.label12.TabIndex = 20;
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(640, 129);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(158, 26);
+            this.label13.TabIndex = 19;
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(640, 92);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(158, 26);
+            this.label14.TabIndex = 18;
+            // 
+            // label15
+            // 
+            this.label15.Location = new System.Drawing.Point(520, 424);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(158, 26);
+            this.label15.TabIndex = 25;
+            // 
+            // label16
+            // 
+            this.label16.Location = new System.Drawing.Point(520, 396);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(158, 26);
+            this.label16.TabIndex = 24;
+            // 
+            // label17
+            // 
+            this.label17.Location = new System.Drawing.Point(520, 361);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(158, 26);
+            this.label17.TabIndex = 23;
+            // 
+            // label18
+            // 
+            this.label18.Location = new System.Drawing.Point(520, 324);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(158, 26);
+            this.label18.TabIndex = 22;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.checkBox3);
@@ -156,6 +308,22 @@
         private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.CheckBox checkBox4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
     }
 }
 
--- Sorting Efficiencies/Form1.cs
+++ Sorting Efficiencies/Form1.cs
@@ -7,19 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Sorting_Efficiencies
 {
     public partial class Form1 : Form
     {
+        Stopwatch myWatch = new Stopwatch();
+        Stopwatch myWatchB = new Stopwatch();
+        Stopwatch myWatchI = new Stopwatch();
         int n;
+        int NumLoop=0;
+        int NumComp = 0;
+        int NumSwap = 0;
+        int NumLoopB = 0;
+        int NumCompB = 0;
+        int NumSwapB = 0;
+        int NumLoopI = 0;
+        int NumCompI = 0;
+        int NumSwapI = 0;
         int[] originalArray;
         int[] sortedArray;
-
-        int[] originalArray2;
         int[] sortedArray2;
-
-        int[] originalArray3;
         int[] sortedArray3;
         public Form1()
         {
@@ -33,15 +42,64 @@ namespace Sorting_Efficiencies
 
             for (int i = 0; i < tempArray.Length; i++)
             {
+                NumLoop = NumLoop + 1;
                 for (int j = i + 1; j < tempArray.Length; j++)
                 {
+                    NumComp = NumComp + 1;
                     if (tempArray[j] < tempArray[i])
                     {
+                        NumSwap = NumSwap + 1;
                         temp = tempArray[i];
                         tempArray[i] = tempArray[j];
                         tempArray[j] = temp;
                     }
                 }
+            }
+        }
+        public void bubble(int[] tempArray)
+        {
+            int bottom = tempArray.Length - 1;
+            int temp;
+            Boolean swap = true;
+
+            while (swap == true)
+            {
+                NumLoopB = NumLoopB + 1;
+                swap = false;
+
+                for (int j = 0; j < bottom; j++)
+                {
+                    NumCompB = NumCompB + 1;
+                    if (tempArray[j] > tempArray[j + 1])
+                    {
+                        NumSwapB = NumSwapB + 1;
+                        swap = true;
+                        temp = tempArray[j];
+                        tempArray[j] = tempArray[j + 1];
+                        tempArray[j + 1] = temp;
+                    }
+                }
+                bottom--;
+            }
+        }
+        public void insertion(int[] tempArray)
+        {
+            int temp, j;
+
+            for (int n = 0; n < tempArray.Length; n++)
+            {
+                NumLoopI = NumLoopI + 1;
+                temp = tempArray[n];
+                j = n - 1;
+
+                while (j >= 0 && tempArray[j] > temp)
+                {
+                    NumCompI = NumCompI + 1;
+                    tempArray[j + 1] = tempArray[j];
+                    j--;
+                }
+                NumSwapI = NumSwapI + 1;
+                tempArray[j + 1] = temp;
             }
         }
         public void Display()
@@ -55,20 +113,6 @@ namespace Sorting_Efficiencies
             {
                 sortedOutput.Text += i + "\n";
             }
-            // foreach (int i in sortedArray)
-            // {
-            //      if (originalArray.Length < n)
-            //      {
-            //         originalOutput.Text += i + "\n";
-            //     }
-            // }
-            // foreach (int i in sortedArray)
-            // {
-            //      if (originalArray.Length < n)
-            //      {
-            //         sortedOutput.Text += i + "\n";
-            //     }
-            // }
         }
 
         private void checkBox3_CheckedChanged(object sender, EventArgs e)
@@ -83,18 +127,23 @@ namespace Sorting_Efficiencies
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
-             n = 10;
+             n = 100;
 
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-             n = 100;
+             n = 10;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
+            originalArray = new int[n];
+            sortedArray = new int[n];
+            sortedArray2 = new int[n];
+            sortedArray3 = new int[n];
+            
             Random randGen = new Random();
             for (int i = 0; i < n; i++)
             {
@@ -103,10 +152,48 @@ namespace Sorting_Efficiencies
             originalArray.CopyTo(sortedArray, 0);
             originalArray.CopyTo(sortedArray2, 0);
             originalArray.CopyTo(sortedArray3, 0);
-            originalArray.CopyTo(originalArray2, 0);
-            originalArray.CopyTo(originalArray3, 0);
+            myWatch.Start();
             Selection(sortedArray);
+            myWatch.Stop();
+            myWatchB.Start();
+            bubble(sortedArray2);
+            myWatchB.Stop();
+            myWatchI.Start();
+            insertion(sortedArray3);
+            myWatchI.Stop();
+            
             Display();
+
+            label7.Text = "Number of loops:" + (NumLoop);
+            label8.Text = "Number of comparisons:" + (NumComp);
+            label9.Text = "Number of shifts" + (NumSwap);
+            label10.Text = "Time in milliseconds:" + Convert.ToString(myWatch.ElapsedMilliseconds);
+
+            label14.Text = "Number of loops:" + (NumLoopB);
+            label13.Text = "Number of comparisons:" + (NumCompB);
+            label12.Text = "Number of shifts" + (NumSwapB);
+            label11.Text = "Time in milliseconds:" + Convert.ToString(myWatchB.ElapsedMilliseconds);
+
+            label18.Text = "Number of loops:" + (NumLoopI);
+            label17.Text = "Number of comparisons:" + (NumCompI);
+            label16.Text = "Number of shifts" + (NumSwapI);
+            label15.Text = "Time in milliseconds:" + Convert.ToString(myWatchI.ElapsedMilliseconds);
+
+
+
+            NumLoop = 0;
+            NumSwap = 0;
+            NumComp = 0;
+            NumLoopB = 0;
+            NumSwapB = 0;
+            NumCompB = 0;
+            NumLoopI = 0;
+            NumSwapI = 0;
+            NumCompI = 0;
+            myWatch.Reset();
+            myWatchB.Reset();
+            myWatchI.Reset();
+
 
         }
     }
